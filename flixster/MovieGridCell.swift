87b26a070a2aaba08fb9250cedf9da455e8f4bff
cs_apple_10 <cs_apple_10@csapple10simac.aamu.edu>
2019-10-24 11:38:39 -0500

    Flixster app creation

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Nehemiah Israel 10/24/19.
+//  Copyright © 2019 Nehemiah Israel. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
